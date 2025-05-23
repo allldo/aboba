d819a31954f81639ca703e6fd4bf8bdc98a8a24c
Allldo <dotan2.maks@mail.ru>
2025-05-23 22:03:27 +0500

    aboba

--- internal/config/db.go
+++ internal/config/db.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jmoiron/sqlx"
@@ -8,9 +9,30 @@ import (
 )
 
 func ConnectDB() (*sqlx.DB, error) {
-	connStr := os.Getenv("DATABASE_URL")
-	if connStr == "" {
-		connStr = "user=postgres password=postgres dbname=mango sslmode=disable"
+	var connStr string
+
+	// Проверяем переменную окружения DATABASE_URL
+	if dbURL := os.Getenv("DATABASE_URL"); dbURL != "" {
+		connStr = dbURL
+	} else {
+		// Получаем параметры из переменных окружения или используем дефолтные
+		host := getEnv("DB_HOST", "localhost")
+		port := getEnv("DB_PORT", "5432")
+		user := getEnv("DB_USER", "postgres")
+		password := getEnv("DB_PASSWORD", "postgres")
+		dbname := getEnv("DB_NAME", "mango")
+		sslmode := getEnv("DB_SSLMODE", "disable")
+
+		connStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			host, port, user, password, dbname, sslmode)
 	}
+
 	return sqlx.Connect("postgres", connStr)
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
